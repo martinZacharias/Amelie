3c47e0a6e68e91207cbfd247c6c0262841ee3da1
Martin Za <pickolo04@gmail.com>
2020-02-13 03:32:41 +0100

    added avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,29 @@
+const Command = require("../models/command.js");
+const Discord = require("discord.js");
+
+class Avatar extends Command {
+	constructor() {
+		super({});
+	}
+	/**
+	 * @param {Discord.Message} msg
+	 * @param {Array<String>} args
+	 */
+	async run(msg, args) {
+		// if msg has mentions use thise, else author
+		const users = msg.mentions.users.size > 0 ? msg.mentions.users.values() : [msg.author];
+		const alreadyPosted = [];
+		for (const user of users) {
+			//check duplicates
+			if (alreadyPosted.includes(user)) continue;
+			alreadyPosted.push(user);
+			const content = new Discord.MessageEmbed()
+				.setAuthor(user.username, user.avatarURL({ size: 32 }))
+				.setColor("2196f3")
+				.setImage(user.avatarURL({ dynamic: true, size: 1024 }));
+			msg.channel.send(content);
+		}
+	}
+}
+
+module.exports = Avatar;
