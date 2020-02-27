db3c537ac8fe5c215209087d0ec9f5f23cf5c208
Martin Za <pickolo04@gmail.com>
2020-02-27 17:13:42 +0100

    changed hentai embed text

--- commands/hentai.js
+++ commands/hentai.js
@@ -27,29 +27,40 @@ class Hentai extends Command {
 	}
 
 	async createContent() {
-		const source = "https://danbooru.donmai.us/posts.json?limit=1&random=true&tags=rating%3Aexplicit";
+		const source = "https://danbooru.donmai.us/posts.json?limit=10&random=true&tags=rating%3Aexplicit";
 		try {
-			let result;
-			do {
-				result = JSON.parse(await WebRequest.getBuffer(source))[0];
-			} while (result.large_file_url === undefined);
+			const result = JSON.parse(await WebRequest.getBuffer(source)).find(img => img.file_url !== undefined);
+			//remove anime names from characters
+			const filteredChars = (result.tag_string_character = result.tag_string_character.replace(/_\([a-z_-]*\)/g, ""));
+			//remove duplicates
+			const characters = new Set(filteredChars.split(" "));
+
+			//add chars to title
+			let title = "";
+			for (const char of characters) title += char + " ";
+			if (title == " ") title = "Unknown Character ";
+			title += `| ${result.tag_string_copyright || "Unknown Anime"}`;
+			//escape underscores to prevent markdown formatting
+			title = title.replace(/_/g, "\\_");
+
 			const content = new Discord.MessageEmbed()
 				.setAuthor(
-					result.tag_string_artist || "no",
+					result.tag_string_artist || "Unknown artist",
 					"",
 					`https://danbooru.donmai.us/posts?tags=${result.tag_string_artist}`
 				)
 				.setColor(0xb303e6) // hentai pink
 				.setURL(`https://danbooru.donmai.us/posts/${result.id}`)
-				.setImage(result.large_file_url)
-				.setTitle(`${result.tag_string_character} | ${result.tag_string_copyright}`)
+				.setImage(result.file_url)
+				.setTitle(title)
 				.setFooter(
-					`🧡${result.fav_count} 🔺${result.up_score}`,
+					`💙${result.fav_count} 🔺${result.up_score}`,
 					"https://cdn.discordapp.com/emojis/674594025692594193.png"
 				)
 				.setTimestamp(result.created_at);
 			return content;
 		} catch (error) {
+			console.error(error);
 			return new Discord.MessageEmbed()
 				.setDescription("an error occured while looking for hentai \n react to try again")
 				.setColor("RED");
