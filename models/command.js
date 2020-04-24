dc434c5572393a8a3020eff69fca8291d320c2b1
MartinZa99 <pickolo04@gmail.com>
2020-04-24 22:30:10 +0200

    enabled NSFW in DMs

--- models/command.js
+++ models/command.js
@@ -25,17 +25,19 @@ class Command {
 	checkFlags(msg) {
 		if (this.options.ownerOnly && !config.owner.includes(msg.author.id))
 			throw new CustomError("Owner Only", "This command can only be used by the owner");
-		if (this.options.nsfwOnly && !msg.channel.nsfw)
+		if (this.options.nsfwOnly && msg.channel.type != "dm" && !msg.channel.nsfw)
 			throw new CustomError("NSFW Only", "This command can only be used in an NSFW channel");
-		if (!this.options.dmOnly && msg.channel.type == "dm")
+		if (!this.options.dmAllowed && msg.channel.type == "dm")
 			throw new CustomError("Invalid Channel Type", "This command cannot be used in DM channels");
 		if (!this.options.guildAllowed && msg.channel.type == "text")
 			throw new CustomError("Invalid Channel Type", "This command cannot be used in guild channels");
-		const missing = msg.member.permissions.missing(this.options.permissions);
-		if (missing.length > 0) {
-			let description = "You're missing:";
-			for (const perm of missing) description += "\n" + perm;
-			throw new CustomError("Insufficient Permissions", description);
+		if (msg.channel.type != "dm") {
+			const missing = msg.member.permissions.missing(this.options.permissions);
+			if (missing.length > 0) {
+				let description = "You're missing:";
+				for (const perm of missing) description += "\n" + perm;
+				throw new CustomError("Insufficient Permissions", description);
+			}
 		}
 		if (this.options.voiceOnly && !msg.member.voice.channel)
 			throw new CustomError("VoiceChannel only", "You need to join a voice channel first");
