518f6d7fe4a6d9c42f64064dd956a0cb8ed577de
martinza99 <mrt.zacharias@gmail.com>
2020-06-12 05:27:41 +0200

    added Message Link to Embed feature

--- handlers/msgHandler.js
+++ handlers/msgHandler.js
@@ -1,5 +1,6 @@
 const config = require("../data/config.js");
 const Discord = require("discord.js");
+const { match } = require("ffmpeg-static");
 /**
  * @class Message handler
  * @property {Discord.Client} client
@@ -19,6 +20,14 @@ class msgHandler {
 	async handle(msg) {
 		//ignore bot messages
 		if (msg.author.bot) return;
+		this.checkMsgLink(msg);
+
+		let logged = false;
+		if (msg.mentions.users.has(this.client.user.id) || msg.channel.type == "dm") {
+			console.log(`${msg.channel.name || "DM"}@${msg.author.username}#${msg.author.discriminator}: ${msg}`);
+			logged = true;
+		}
+
 		//check if msg starts with prefix and return found prefix
 		const prefix = config.prefix.find((pre) => msg.content.startsWith(pre));
 		if (prefix === undefined) return;
@@ -30,6 +39,8 @@ class msgHandler {
 
 		let cmd = args.shift();
 		if (!this.commands.hasOwnProperty(cmd)) return;
+		if (!logged) console.log(`${msg.channel.name}@${msg.author.username}#${msg.author.discriminator}: ${msg}`);
+
 		cmd = new this.commands[cmd]();
 
 		try {
@@ -40,6 +51,49 @@ class msgHandler {
 			if (!error.show) throw error;
 			await showError(msg, error);
 		}
+	}
+
+	/**
+ 	* turns message links into embeds
+ 	* @param {Discord.Message} msg
+ 	*/
+	async checkMsgLink(msg) {
+		const messageLinkPattern = /^https?:\/\/discord(app)?\.com\/channels\/(\d{17,19})\/(\d{17,19})\/(\d{17,19})$/i;
+		const matches = msg.content.match(messageLinkPattern);
+		if (matches === null) return;
+		try {
+			const guild = new Discord.Guild(this.client, { id: matches[2] });
+			const channel = await new Discord.TextChannel(guild, { id: matches[3] }).fetch();
+			const qMsg = await new Discord.Message(this.client, { id: matches[4] }, channel).fetch();
+			msg.delete({ reason: "replaced with embed" });
+
+			let reactionStr = "";
+			for (const [key, reaction] of qMsg.reactions.cache) {
+				const emoji = reaction.emoji;
+				// default emojis have no id and avaibalbe not set
+				if (!emoji.id || emoji.available) {
+					if (emoji.id)
+						reactionStr += `<:${emoji.identifier}>`;
+					else
+						reactionStr += emoji.name;
+				}
+			}
+
+			const embem = new Discord.MessageEmbed()
+				.setColor(0x006666)
+				.setDescription(`[Link to Message](${matches[0]})\n${qMsg.content}`)
+				.setAuthor(qMsg.author.username, qMsg.author.avatarURL())
+				.setFooter(qMsg.channel.name)
+				.setTimestamp(qMsg.createdTimestamp);
+
+			if (reactionStr.length > 0)
+				embem.addField("Reactions", reactionStr);
+
+			msg.channel.send(embem);
+		} catch (error) {
+			msg.react("❓");
+		}
+
 	}
 }
 
