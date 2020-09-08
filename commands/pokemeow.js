612f36d766fa54fb9319e4b275ef8db7d6a008ce
martinza99 <mrt.zacharias@gmail.com>
2020-09-08 22:46:19 +0200

    added pokemeow cooldown

--- commands/pokemeow.js
+++ commands/pokemeow.js
@@ -4,7 +4,9 @@ const Discord = require("discord.js");
 const delay = process.env.poketime || 10;
 
 class Pokemeow extends Command {
+	static active = false;
 	static checkMatch(msg) {
+		if (this.active) return null;
 		return msg.guild.id === "550777230066712611" && msg.content === ";p";
 	}
 	/**
@@ -12,10 +14,12 @@ class Pokemeow extends Command {
 	 * @param {Array<String>} args
 	 */
 	async run(msg, args) {
+		Pokemeow.active = true;
 		setTimeout(() => {
 			msg.channel.send(
 				`${msg.member.displayName}, ${delay} seconds have passed`
 			);
+			Pokemeow.active = false;
 		}, delay * 1000);
 	}
 }
