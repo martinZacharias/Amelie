2d990d1957c6a35091c729c42d1c6d82d743d1bf
martinza99 <mrt.zacharias@gmail.com>
2021-02-24 02:09:16 +0100

    fixed class name

--- commands/basic/pokefish.js
+++ commands/basic/pokefish.js
@@ -3,10 +3,10 @@ const Command = require("../../models/command");
 
 const delay = process.env.pokefish;
 
-class Pokemeow extends Command {
+class Pokefish extends Command {
 	static active = false;
 	static checkMatch(msg) {
-		// TODO: guild has pokemeow 664508672713424926
+		// TODO: guild has pokefish 664508672713424926
 		if (this.active) return null;
 		return msg.guild.id === "550777230066712611" && msg.content === ";f";
 	}
@@ -15,14 +15,14 @@ class Pokemeow extends Command {
 	 * @param {Array<String>} args
 	 */
 	async run(msg, args) {
-		Pokemeow.active = true;
+		Pokefish.active = true;
 		setTimeout(() => {
 			msg.channel.send(
 				`${msg.member.displayName}, ${delay} seconds have passed`
 			);
-			Pokemeow.active = false;
+			Pokefish.active = false;
 		}, delay * 1000);
 	}
 }
 
-module.exports = Pokemeow;
+module.exports = Pokefish;
