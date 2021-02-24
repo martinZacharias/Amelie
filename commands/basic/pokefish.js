cb79e3abb62431b7c30b93aabe771740bf1a061c
martinza99 <mrt.zacharias@gmail.com>
2021-02-24 01:59:48 +0100

    added pokefish

---
+++ commands/basic/pokefish.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const Command = require("../../models/command");
+
+const delay = process.env.pokefish;
+
+class Pokemeow extends Command {
+	static active = false;
+	static checkMatch(msg) {
+		// TODO: guild has pokemeow 664508672713424926
+		if (this.active) return null;
+		return msg.guild.id === "550777230066712611" && msg.content === ";f";
+	}
+	/**
+	 * @param {Discord.Message} msg
+	 * @param {Array<String>} args
+	 */
+	async run(msg, args) {
+		Pokemeow.active = true;
+		setTimeout(() => {
+			msg.channel.send(
+				`${msg.member.displayName}, ${delay} seconds have passed`
+			);
+			Pokemeow.active = false;
+		}, delay * 1000);
+	}
+}
+
+module.exports = Pokemeow;
